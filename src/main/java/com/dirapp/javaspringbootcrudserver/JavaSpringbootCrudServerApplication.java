fe6f1c6b68e7936b8bb707b4002bcc280e62dba9
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-03-07 06:27:03 +0700

    backup

---
+++ src/main/java/com/dirapp/javaspringbootcrudserver/JavaSpringbootCrudServerApplication.java
@@ -0,0 +1,13 @@
+package com.dirapp.javaspringbootcrudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootCrudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootCrudServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dirapp/javaspringbootcrudserver/JavaSpringbootCrudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirapp.javaspringbootcrudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootCrudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
